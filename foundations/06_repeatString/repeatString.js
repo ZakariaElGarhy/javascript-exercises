9697c48d537cba7405034b037a5168fdab6d0c1e
xLeeBro <168808049+ZakariaElGarhy@users.noreply.github.com>
2026-07-29 11:22:11 +0300

    Update repeatString.js

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str , num) {
+  let huh = [];
+  for (let i = 0; i < num; i++){
+    huh.push(str);
+  }
+  let newstr = huh.join("");
+  return newstr;
 };
 
+let str= prompt("banana");
+let num = +prompt("OKBRO");
+
+alert(repeatString())
 // Do not edit below this line
 module.exports = repeatString;
